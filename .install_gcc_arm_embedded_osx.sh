7c891311f30b4347588e37f4f559e784a80102fd
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-03 21:52:22 +0100

    Add installation script for OS X (travis)
    Adjust travis build file

---
+++ .install_gcc_arm_embedded_osx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is taken mainly from https://gist.github.com/joegoggins/7763637 
+
+wget -O gcc-arm-none-eabi-6-2017-q1-update-mac.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-mac.tar.bz2?product=GNU%20ARM%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,6-2017-q1-update
+
+# Decompress
+tar xf gcc-arm-none-eabi-6-2017-q1-update-mac.tar.bz2
+
+# Make a place to install it to
+mkdir /usr/local/gcc_arm
+
+# Move the unzipped stuff there.
+mv gcc-arm-none-eabi-6-2017-q1-update/ /usr/local/gcc_arm/
+
+# Change your PATH by appending the "bin" folder of the folder
+# NOTE: You should also add this line to ~/.bashrc or ~/.bash_profile (in your home directory)
+# so you don't have to remember to do this every time you want to compile firmware.
+export PATH="$PATH:/usr/local/gcc_arm/gcc-arm-none-eabi-6-2017-q1-update/bin"
+
+# Check to ensure the gcc-arm firmware compiler is available (so `make clean dependents all` will work.)
+arm-none-eabi-gcc --version

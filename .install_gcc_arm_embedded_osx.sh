a37723731cd6d7c2bb3da6467a5ee987ca517aee
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-04 09:21:05 +0100

    Add path to compiler to PATH

--- .install_gcc_arm_embedded_osx.sh
+++ .install_gcc_arm_embedded_osx.sh
@@ -14,8 +14,9 @@ mkdir /usr/local/gcc_arm
 mv gcc-arm-none-eabi-6-2017-q1-update/ /usr/local/gcc_arm/
 
 # Change your PATH by appending the "bin" folder of the folder
-# NOTE: You should also add this line to ~/.bashrc or ~/.bash_profile (in your home directory)
-# so you don't have to remember to do this every time you want to compile firmware.
+# NOTE:
+# You should also add this line to ~/.bashrc or ~/.bash_profile (in your home directory)
+# It needs also be set in .travis.yml
 export PATH="$PATH:/usr/local/gcc_arm/gcc-arm-none-eabi-6-2017-q1-update/bin"
 
 # Check to ensure the gcc-arm firmware compiler is available (so `make clean dependents all` will work.)

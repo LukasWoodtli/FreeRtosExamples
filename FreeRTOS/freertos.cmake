0c78a4ae6bffde15c0d3bc6614d838b72c2d48ed
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-17 08:37:18 +0100

    Small fixes

--- FreeRTOS/freertos.cmake
+++ FreeRTOS/freertos.cmake
@@ -9,13 +9,15 @@ include_directories(${CMAKE_CURRENT_LIST_DIR}
 	${CMAKE_CURRENT_LIST_DIR}/Source/portable/GCC/ARM_CM3)
 
 # Source files
-FILE(GLOB_RECURSE FREERTOS_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
+FILE(GLOB FREERTOS_SOURCES ${CMAKE_CURRENT_LIST_DIR}/Source/*.c)
+FILE(GLOB FREERTOS_PORTABLE_SOURCES ${CMAKE_CURRENT_LIST_DIR}/Source/portable/GCC/ARM_CM3/*.c)
 
-set(FREERTOS_MEM_MANG  ${CMAKE_CURRENT_LIST_DIR}/Source/portable/MemMang/heap_1.c)
+
+set(FREERTOS_MEM_MANG  ${CMAKE_CURRENT_LIST_DIR}/Source/portable/MemMang/heap_2.c)
 ##### Outputs #####
 
 # Create emlib library
-add_library(freertoslib ${FREERTOS_SOURCES} ${FREERTOS_MEM_MANG})
+add_library(freertoslib ${FREERTOS_SOURCES} ${FREERTOS_PORTABLE_SOURCES} ${FREERTOS_MEM_MANG})
 
 # Add library to build
 set(LIBS ${LIBS} freertoslib)

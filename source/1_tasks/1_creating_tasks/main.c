2b72281917eb0a24fc4a5a58bc30fc9906430c46
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-24 22:32:57 +0100

    Add code for printing to console in Simplicity Studio

--- source/1_tasks/1_creating_tasks/main.c
+++ source/1_tasks/1_creating_tasks/main.c
@@ -14,6 +14,53 @@
 
 #include "sleep.h"
 
+void setupSWOForPrint(void)
+
+{
+
+  /* Enable GPIO clock. */
+  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+
+  /* Enable Serial wire output pin */
+  GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
+
+
+#if defined(_EFM32_GIANT_FAMILY) || defined(_EFM32_LEOPARD_FAMILY) || defined(_EFM32_WONDER_FAMILY)
+
+  /* Set location 0 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
+
+  /* Enable output on pin - GPIO Port F, Pin 2 */
+  GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
+  GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
+#else
+  /* Set location 1 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) |GPIO_ROUTE_SWLOCATION_LOC1;
+
+  /* Enable output on pin */
+  GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
+  GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
+#endif
+
+  /* Enable debug clock AUXHFRCO */
+  CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
+
+  /* Wait until clock is ready */
+  while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY));
+
+  /* Enable trace in core debug */
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+  ITM->LAR  = 0xC5ACCE55;
+  ITM->TER  = 0x0;
+  ITM->TCR  = 0x0;
+  TPI->SPPR = 2;
+  TPI->ACPR = 0xf;
+  ITM->TPR  = 0x0;
+  DWT->CTRL = 0x400003FE;
+  ITM->TCR  = 0x0001000D;
+  TPI->FFCR = 0x00000100;
+  ITM->TER  = 0x1;
+}
 
 void vPrintString(char const * const str) {
     char const * i = str;
@@ -82,7 +129,8 @@ int main(void)
 #endif
 
 
-    
+  setupSWOForPrint();
+
   xTaskCreate(vTask1,   // pointer to the task function
               "Task 1", // task name
               240,      // stack size in words

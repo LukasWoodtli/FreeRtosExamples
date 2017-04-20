8abe9b3b1144579c584c83958e5e5982b9478025
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-04-20 08:34:46 +0200

    Search for additional build tools (objdump, objcopy ...)

--- toolchain/arm-gcc.cmake
+++ toolchain/arm-gcc.cmake
@@ -21,11 +21,36 @@ set(CMAKE_CXX_COMPILER_WORKS 1)
 
 set(CMAKE_ASM_COMPILER ${COMPILER_PREFIX}arm-none-eabi-g++)
 
+# pathes to search other tools
+set(TOOLS_SEARCH_PATHS
+  $ENV{PATH}
+  ${COMPILER_PREFIX}
+  /usr/local/gcc_arm/gcc-arm-none-eabi-5_4-2016q3/bin/ # my installation path on OS X
+)
+
+
 # Set other tools
-set(OBJSIZE ${COMPILER_PREFIX}arm-none-eabi-size)
-set(OBJCOPY ${COMPILER_PREFIX}arm-none-eabi-objcopy)
-set(OBJDUMP ${COMPILER_PREFIX}arm-none-eabi-objdump)
-set(DEBUGGER ${COMPILER_PREFIX}arm-none-eabi-gdb)
+FIND_PROGRAM(OBJSIZE arm-none-eabi-size
+  PATHS
+  ${TOOLS_SEARCH_PATHS}
+)
+
+
+FIND_PROGRAM(OBJCOPY arm-none-eabi-objcopy
+  PATHS
+  ${TOOLS_SEARCH_PATHS}
+)
+
+
+FIND_PROGRAM(OBJDUMP arm-none-eabi-objdump
+  PATHS
+  ${TOOLS_SEARCH_PATHS}
+)
+
+FIND_PROGRAM(DEBUGGER arm-none-eabi-gdb
+  PATHS
+  ${TOOLS_SEARCH_PATHS}
+)
 
 # Remove preset linker flags
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "") 
